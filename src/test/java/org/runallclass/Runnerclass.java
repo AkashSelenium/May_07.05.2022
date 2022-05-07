fec8550e49ab60b1e12b382f5f7a7baf4a056294
AkashSelenium <Akashsmart873@gmail.com>
2022-05-07 20:32:07 +0530

    Few Lines Added

--- src/test/java/org/runallclass/Runnerclass.java
+++ src/test/java/org/runallclass/Runnerclass.java
@@ -6,7 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\Features\\Login.Feature",dryRun = true)
+@CucumberOptions(features = "src\\test\\resources\\Features\\Login.Feature",dryRun = true,
+monochrome= false)
 public class Runnerclass {
 
 	
