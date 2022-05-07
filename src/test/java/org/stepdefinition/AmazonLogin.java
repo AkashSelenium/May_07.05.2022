11a0b9687805d80a10f82327f2ff0cafa8a43415
AkashSelenium <Akashsmart873@gmail.com>
2022-05-07 20:27:25 +0530

    My Project is Added

---
+++ src/test/java/org/stepdefinition/AmazonLogin.java
@@ -0,0 +1,26 @@
+package org.stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AmazonLogin {
+
+	@Given("Using Amazon Url user have to open Amazon Login Page")
+	public void using_Amazon_Url_user_have_to_open_Amazon_Login_Page() {
+	}
+
+	@When("Using Valid Email Valid Password")
+	public void using_Valid_Email_Valid_Password() {
+	}
+
+	@When("Click ON Login button")
+	public void click_ON_Login_button() {
+	}
+
+	@Then("Homepage Should be Displayed")
+	public void homepage_Should_be_Displayed() {
+	}
+
+
+}
