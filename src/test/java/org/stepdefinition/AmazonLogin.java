0d1f9bb6b671f00efd6b1e21ddc2785ee4997b36
AkashSelenium <Akashsmart873@gmail.com>
2022-05-08 15:14:45 +0530

    Few Lines Code is Added

--- src/test/java/org/stepdefinition/AmazonLogin.java
+++ src/test/java/org/stepdefinition/AmazonLogin.java
@@ -1,13 +1,33 @@
 package org.stepdefinition;
 
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AmazonLogin {
 
+	public static WebDriver Driver;
+
+	@BeforeClass
+	private void Method1() {
+		Driver = new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
+		Driver.get(
+				"https://www.amazon.in/?&ext_vrnc=hi&tag=googinhydr1-21&ref=pd_sl_4b3e4wrxds_e&adgrpid=60639568242&hvpone=&hvptwo=&hvadid=294102729729&hvpos=&hvnetw=g&hvrand=2005791130408263198&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1007809&hvtargid=kwd-316976912080&hydadcr=5841_1914865&gclid=EAIaIQobChMIoK23w83P9wIVHEBIAB09qQ8yEAAYASAAEgI8evD_BwE");
+		
+	}
+
 	@Given("Using Amazon Url user have to open Amazon Login Page")
 	public void using_Amazon_Url_user_have_to_open_Amazon_Login_Page() {
+		Driver.findElement(By.name(""));
+		
 	}
 
 	@When("Using Valid Email Valid Password")
@@ -21,6 +41,5 @@ public class AmazonLogin {
 	@Then("Homepage Should be Displayed")
 	public void homepage_Should_be_Displayed() {
 	}
-
 
 }
