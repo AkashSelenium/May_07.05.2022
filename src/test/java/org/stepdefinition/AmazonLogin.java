f11e7f50f53ee71433a1f7c1012cab6490e50d0f
AkashSelenium <Akashsmart873@gmail.com>
2022-05-08 15:19:38 +0530

    Few Lined Added A

--- src/test/java/org/stepdefinition/AmazonLogin.java
+++ src/test/java/org/stepdefinition/AmazonLogin.java
@@ -8,18 +8,22 @@ public class AmazonLogin {
 
 	@Given("Using Amazon Url user have to open Amazon Login Page")
 	public void using_Amazon_Url_user_have_to_open_Amazon_Login_Page() {
+		System.out.println("Test Method 1");
 	}
 
 	@When("Using Valid Email Valid Password")
 	public void using_Valid_Email_Valid_Password() {
+		System.out.println("Test Method 2");
 	}
 
 	@When("Click ON Login button")
 	public void click_ON_Login_button() {
+		System.out.println("Test Method 3");
 	}
 
 	@Then("Homepage Should be Displayed")
 	public void homepage_Should_be_Displayed() {
+		System.out.println("Test Method 4");
 	}
 
 
